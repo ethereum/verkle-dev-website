de671b31242f97fe4b6882214d5fe542d288a3a9
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2022-08-11 18:23:08 -0600

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/controlc/Cave/Ethereum/verkle-dev-website/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/controlc/Cave/Ethereum/verkle-dev-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/controlc/Cave/Ethereum/verkle-dev-website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/controlc/Cave/Ethereum/verkle-dev-website/src/css/custom.css'),
-];
