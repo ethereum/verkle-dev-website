61a2797c790556b1c6ce429c19c1ad1c853c8154
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-08-24 20:05:13 +0100

    add an introduction header, but leave it empty for now

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,8 +75,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Introduction',
+                to: '/',
               },
             ],
           },
