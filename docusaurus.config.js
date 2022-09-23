114c990e644a8e0f34a434e728c4416d95809943
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-09-23 22:12:33 +0100

    add /blob/master to editUrl link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,7 +36,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ethereum/verkle-dev-website',
+            'https://github.com/ethereum/verkle-dev-website/blob/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
